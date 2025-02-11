345ba619dd9ed7f9d425c7292ebcdf6fa7b4a4fc
Gaurav Singh <gksingh947@gmail.com>
2025-02-11 16:10:06 +0530

    App.js Created

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app=express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // ✅ Allow all necessary methods
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"], // ✅ Allow cookies
+}));
+
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({extended:true,limit:'16kb'}));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+// routes 
