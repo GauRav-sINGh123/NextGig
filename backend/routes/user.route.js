aa5e3859e473c62b7e04cb6815d9ed523ce5b77a
Gaurav Singh <gksingh947@gmail.com>
2025-02-12 13:51:41 +0530

    Methods Added in User Route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express"
+import { currentUser, login, logout, signup } from "../controllers/user.controller.js"
 const router=express.Router()
 
-router.route("/signup").post()
+router.route("/signup").post(signup)
 
-router.route("/login").post()
+router.route("/login").post(login)
 
-router.route("/logout").post()
+router.route("/logout").post(logout)
 
-router.route("/current_user").get()
+router.route("/current_user").get(currentUser)
 
 export default router
