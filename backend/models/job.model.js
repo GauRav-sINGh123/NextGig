84538372071971d60ff7d0de50b75a6b6a74ca51
Gaurav Singh <gksingh947@gmail.com>
2025-02-12 13:19:03 +0530

    Job Model Created

---
+++ backend/models/job.model.js
@@ -0,0 +1,49 @@
+import {Schema,model} from "mongoose";
+
+const jobSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requirements: [{
+        type: String
+    }],
+    salary: {
+        type: Number,
+        required: true
+    },
+    experience:{
+        type:Number,
+        required:true,
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    jobType: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
+},{timestamps:true});
+
+export const Job = model("Job", jobSchema);
