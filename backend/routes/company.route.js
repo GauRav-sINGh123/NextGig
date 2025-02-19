61982975aafc7289056ac739c57057bd4c0fbd6c
Gaurav Singh <gksingh947@gmail.com>
2025-02-19 15:36:28 +0530

    Route Request Type Changed

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -9,7 +9,7 @@ router.route("/get_companies").get(isAuthenticated,getAllCompanies)
 
 router.route("/get_company/:id").get(isAuthenticated,getCompanyById)
 
-router.route("/update_company/:id").put(isAuthenticated,updateCompany)
+router.route("/update_company/:id").patch(isAuthenticated,updateCompany)
 
 router.route("/delete_company/:id").delete(isAuthenticated,deleteCompany)
 
