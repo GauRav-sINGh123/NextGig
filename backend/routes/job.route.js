637b652fa586c433ed145e177b46efcd46c495a1
Gaurav Singh <gksingh947@gmail.com>
2025-02-22 23:42:30 +0530

    Get all jobs Route added

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router=express.Router();
 import isAuthenticated from "../middleware/isAuthenticated.js";
-import {createJob,deleteJob, getAllJobByAdmin, getJobById} from '../controllers/job.controller.js'
+import {createJob,deleteJob, getAllJobByAdmin, getAllJobs, getJobById} from '../controllers/job.controller.js'
 
 router.route('/create_job').post(isAuthenticated,createJob);
+
+router.route('/get_all_jobs').get(isAuthenticated,getAllJobs);
 
 router.route('/delete_job/:id').delete(isAuthenticated,deleteJob);
 
