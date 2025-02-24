3874a4fd1ac798ccd725e6aad2fc142570f56926
Gaurav Singh <gksingh947@gmail.com>
2025-02-24 23:28:11 +0530

    Apply Job Route Added

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,5 +1,7 @@
 import express from "express"
 const router=express.Router();
+import { applyJob } from "../controllers/application.controller.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 
-
+router.route('/apply/:id').post(isAuthenticated,applyJob);
 export default router
