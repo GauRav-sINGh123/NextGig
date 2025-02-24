ff59398d6b5f610f088c4ba631ec50b1163d699e
Gaurav Singh <gksingh947@gmail.com>
2025-02-24 23:38:46 +0530

    Applicant Route Added

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 const router=express.Router();
-import { applyJob ,getAppliedJobs} from "../controllers/application.controller.js";
+import { applyJob ,getApplicants,getAppliedJobs} from "../controllers/application.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 router.route('/apply/:id').post(isAuthenticated,applyJob);
 
 router.route('/get_applied_jobs').get(isAuthenticated,getAppliedJobs);
+
+router.route('/get_applicants/:id').get(isAuthenticated,getApplicants);
 
 export default router
