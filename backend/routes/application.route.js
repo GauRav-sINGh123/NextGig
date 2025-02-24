7b6040a0f9a9000efb27bb4fa0fb471d1b2db831
Gaurav Singh <gksingh947@gmail.com>
2025-02-24 23:43:14 +0530

    Update Application Status Route Added

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 const router=express.Router();
-import { applyJob ,getApplicants,getAppliedJobs} from "../controllers/application.controller.js";
+import { applyJob ,getApplicants,getAppliedJobs, updateApplicationStatus} from "../controllers/application.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 router.route('/apply/:id').post(isAuthenticated,applyJob);
@@ -8,5 +8,7 @@ router.route('/apply/:id').post(isAuthenticated,applyJob);
 router.route('/get_applied_jobs').get(isAuthenticated,getAppliedJobs);
 
 router.route('/get_applicants/:id').get(isAuthenticated,getApplicants);
+
+router.route('/update/status/:id').post(isAuthenticated,updateApplicationStatus);
 
 export default router
