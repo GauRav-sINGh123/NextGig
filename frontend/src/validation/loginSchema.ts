2709bf75b2cb4ca1ef3e7ad768c0b64a239b7343
Gaurav Singh <gksingh947@gmail.com>
2025-02-27 22:12:31 +0530

    Login Schema Password Message Added

--- frontend/src/validation/loginSchema.ts
+++ frontend/src/validation/loginSchema.ts
@@ -3,5 +3,5 @@ import {z} from 'zod'
 
 export const loginSchema=z.object({
     email:z.string().email(),
-    password:z.string().min(8)
+    password:z.string().min(8,{message:"Password must be at least 8 characters long"}),
 })
