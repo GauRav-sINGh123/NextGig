c22c1a199cfbcce1b6b533b23e4ffef31c30ea51
Gaurav Singh <gksingh947@gmail.com>
2025-02-28 23:38:26 +0530

    Multer Added To Update User Route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { currentUser, login, logout, signup,updateUser } from "../controllers/user.controller.js"
 import isAuthenticated from "../middleware/isAuthenticated.js"
+import {upload} from "../middleware/multer.js"
 const router=express.Router()
 
 router.route("/signup").post(signup)
@@ -9,7 +10,7 @@ router.route("/login").post(login)
 
 router.route("/logout").post(logout)
 
-router.route("/update_user").put(isAuthenticated,updateUser)
+router.route("/update_user").put(isAuthenticated,upload,updateUser)
 
 router.route("/current_user").get(isAuthenticated,currentUser)
 
