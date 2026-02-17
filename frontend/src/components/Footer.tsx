7cbf8565996a90f90a8b4283fa6cd9e9885f4697
Gaurav Singh <gksingh947@gmail.com>
2026-02-17 12:52:25 +0530

    Minimal Footer change

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-    <div className='bg-inherit text-center py-4 mt-4'>2025. All rights reserved</div>
+    <div className='bg-inherit text-center py-4 mt-4'>2026. All rights reserved</div>
   )
 }
 
