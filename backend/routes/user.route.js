5fff7dbd580cad37e8505f8bb4e415fb45713990
Gaurav Singh <gksingh947@gmail.com>
2026-02-21 14:36:29 +0530

    Update profile picture route add

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { currentUser, login, logout, signup,updateUser } from "../controllers/user.controller.js"
+import { currentUser, login, logout, signup,updateUserProfile,updateUserProfilePicture } from "../controllers/user.controller.js"
 import isAuthenticated from "../middleware/isAuthenticated.js"
 import {upload} from "../middleware/multer.js"
 const router=express.Router()
@@ -10,8 +10,10 @@ router.route("/login").post(login)
 
 router.route("/logout").post(logout)
 
-router.route("/update_user").put(isAuthenticated,upload,updateUser)
+router.route("/update_user").put(isAuthenticated,upload,updateUserProfile)
 
 router.route("/current_user").get(isAuthenticated,currentUser)
+
+router.route("/update_user_profile_image").get(isAuthenticated,updateUserProfilePicture)
 
 export default router
